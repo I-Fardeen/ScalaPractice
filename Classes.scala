8205bed526bc2b1302602f291d823c3f74b3d9e9
Fardeen A Khan <79132148+I-Fardeen@users.noreply.github.com>
2022-12-06 06:34:57 +0000

    Copied from local directory to Github

--- Classes.scala
+++ Classes.scala
@@ -1,26 +1,30 @@
 object Classes{
-    trait Cart:
+    trait Cart
+    {
         var total: Int
         var nitems: Int
         def addItem(nitems: Int): Boolean
-        def getTotal(): Double
-    class CheckOut extends Cart{
-            var total = 0;
-            var nitems = 0;
-            var temp=0;
-        override def addItem(nitems: Int): Boolean = {
-            if nitems > 0 then
-            temp = nitems;
-            temp = temp + 1;
-            total = total + nitems
-            true
-        }
-        override def getTotal(): Double = {
-            println(s"Cart total = ${total}");
-            total
-        }
+        def getTotal(): Double;
     }
-    def main(args: Array[String]) = {
+        class CheckOut extends Cart
+        {
+                var total = 0;
+                var nitems = 1;
+                var temp=0;
+                override def addItem(nitems: Int): Boolean = {
+                if nitems > 0 then
+                temp = nitems;
+                temp = temp + 1;
+                total = total + nitems
+                true
+                }
+                override def getTotal(): Double = {
+                println(s"Cart total = ${total}");
+                total
+                }
+        }
+    def main(args: Array[String]) = 
+    {
         val check = CheckOut();
         val c = check.addItem(5);
         if c then
